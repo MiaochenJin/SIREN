625d8bc59da0caa693a305e81ab30a8b710e99d9
Nicholas Kamp <nkamp@fas.harvard.edu>
2024-04-25 18:00:43 -0400

    cleanup changes that should be ported to the main branch before publication

--- projects/injection/private/Injector.cxx
+++ projects/injection/private/Injector.cxx
@@ -363,7 +363,7 @@ double Injector::SecondaryGenerationProbability(std::shared_ptr<LI::dataclasses:
 
 double Injector::GenerationProbability(LI::dataclasses::InteractionTree const & tree) const {
     double probability = 1.0;
-    std::set<std::shared_ptr<LI::dataclasses::InteractionTreeDatum>>::const_iterator it = tree.tree.cbegin();
+    std::vector<std::shared_ptr<LI::dataclasses::InteractionTreeDatum>>::const_iterator it = tree.tree.cbegin();
     while(it != tree.tree.cend()) {
         if((*it)->depth()==0) probability *= GenerationProbability((*it));
         else probability *= SecondaryGenerationProbability((*it));
